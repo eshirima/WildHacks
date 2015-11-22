6711ec71c5308cd276bd28b49953c08539ec17a2
Emil Shirima <emil.shirima@hotmail.com>
2015-11-22 10:19:00 -0600

    Changed the port number

--- app.js
+++ app.js
@@ -59,7 +59,9 @@ app.post('/process', parseUrlEnconded, function (request, response) {
 
 });
 
-app.listen(3000, function () {
+var port = process.env.port || 3000;
+
+    app.listen(port, function () {
   console.log('Listening on port 3000');
 });
 
